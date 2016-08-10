ebfd804b9c8749175bfe1f8c5f7d88dc7fb638d8
Dean Scelza and Kevin Johnston <dean@>
2016-08-10 11:01:35 -0700

    add branch for bmi and temp calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,32 @@
-// Arithmetic practice
+$(function(){
+	// BMI Calculator
+	var weight = parseInt(prompt("Enter your weight in pounds: "));
+	var height = parseInt(prompt("Enter your height in inches: "));
 
-// var add = function(number1,number2){
-// return number1 + number2;
-// };
-//
-// var subtract = function(number1,number2){
-// return number1 - number2;
-// };
-//
-// var multiply = function(number1,number2){
-// return number1 * number2;
-// };
-//
-// var divide = function(number1,number2){
-// return number1 / number2;
-//};
-//
-// var number1 = parseInt(prompt("Enter a number: "));
-// var number2 = parseInt(prompt("Enter another number: "));
-//
-// var sum = add(number1, number2);
-// alert("The sum is " + sum);
-//
-// var difference = subtract(number1, number2);
-// alert("The difference is " + difference);
-//
-// var product = multiply(number1, number2);
-// alert("The product is " + product);
-//
-// var quotient = divide(number1, number2);
-// alert("The quotient is " + quotient);
+	var calculate = function(weight, height) {
+		return (703 * weight) / Math.pow(height, 2);
+	};
 
-// BMI Calculator
+	var bmi = calculate(weight, height);
+	alert("Your bmi is " + bmi);
 
-var weight = parseInt(prompt("Enter your weight in pounds: "));
-var height = parseInt(prompt("Enter your height in inches: "));
+	// Temperature converter
+	var celsius = parseInt(prompt("Enter the Temperature in Celsius:"));
+	var fahrenheit = parseInt(prompt("Enter the Temperature in Fahrenheit:"));
 
-var calculate = function(weight, height) {
-	return (703 * weight) / Math.pow(height, 2);
-};
+	var tempF = function(celsius){
+	  return (celsius * 1.8) + 32;
+	};
 
-var bmi = calculate(weight, height);
-alert("Your bmi is " + bmi);
+	var tempC = function(fahrenheit){
+	  return (fahrenheit - 32) / 1.8;
+	};
 
-// Temperature converter
-var celsius = parseInt(prompt("Enter the Temperature in Celsius:"));
-var fahrenheit = parseInt(prompt("Enter the Temperature in Fahrenheit:"));
 
-var tempF = function(celsius){
-  return (celsius * 1.8) + 32;
-};
+	var tempFFinal = tempF(celsius);
+	alert(tempFFinal + "F");
+	var tempCFinal = tempC(fahrenheit);
+	alert(tempCFinal + "C");
 
-var tempC = function(fahrenheit){
-  return (fahrenheit - 32) / 1.8;
-};
 
-var tempCFinal = tempC(fahrenheit);
-alert(tempCFinal + "C");
-
-var tempFFinal = tempF(celsius);
-alert(tempFFinal + "F");
+});
