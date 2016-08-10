86554db95718624e5a0b9e77f675dfb586b4b13f
Dean Scelza and Kevin Johnston <dean@>
2016-08-10 13:25:40 -0700

    Add functional subtract, multiply, and divide UI features

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,31 @@ $(function(){
 		event.preventDefault();
 		var number1 = parseInt($("#add1").val());
 		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#addOutput").text("The sum is " + result);
 
-		alert("The sum is " + add(number1, number2));
 	});
-
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#subtract1").val());
+		var number2 = parseInt($("#subtract2").val());
+		var result = subtract(number1, number2);
+		$("#subtractOutput").text("The difference is " + result);
+	});
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#multiply1").val());
+		var number2 = parseInt($("#multiply2").val());
+		var result = multiply(number1, number2);
+		$("#multiplyOutput").text("The product is " + result);
+	});
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#divide1").val());
+		var number2 = parseInt($("#divide2").val());
+		var result = divide(number1, number2);
+		$("#divideOutput").text("The quotient is " + result);
+	});
 
 
 });
