9946df3008c2686de5a01ee86fbfbe259031fe3b
Andrew Merrell and Kevin Johnston <kevin@>
2016-08-11 14:44:01 -0700

    Finish calculator with branching exercise.

--- js/scripts.js
+++ js/scripts.js
@@ -24,39 +24,18 @@ $(function(){
 		var number1 = parseInt($("#input1").val());
 		var number2 = parseInt($("#input2").val());
 		var operator = $("input:radio[name=operator]:checked").val();
+		var result;
+		 if (operator === "add") {
+			 result = add(number1, number2);
+		 	} else if (operator === "subtract") {
+			result = subtract(number1, number2);
+			} else if (operator === "multiply") {
+				result = multiply(number1, number2);
+			} else if (operator === "divide") {
+				result = divide(number1, number2);
+			}
+
 		$("#output").text(result);
 
 	});
-	$("form#subtract").submit(function(event) {
-		event.preventDefault();
-		var number1 = parseInt($("#subtract1").val());
-		var number2 = parseInt($("#subtract2").val());
-		var result = subtract(number1, number2);
-		$("#subtractOutput").text("The difference is " + result);
-	});
-	$("form#multiply").submit(function(event) {
-		event.preventDefault();
-		var number1 = parseInt($("#multiply1").val());
-		var number2 = parseInt($("#multiply2").val());
-		var result = multiply(number1, number2);
-		$("#multiplyOutput").text("The product is " + result);
-	});
-	$("form#divide").submit(function(event) {
-		event.preventDefault();
-		var number1 = parseInt($("#divide1").val());
-		var number2 = parseInt($("#divide2").val());
-		var result = divide(number1, number2);
-		$("#divideOutput").text("The quotient is " + result);
-	});
-
-
 });
-
-// var difference = subtract(number1, number2);
-// alert("The difference is " + difference);
-//
-// var product = multiply(number1, number2);
-// alert("The product is " + product);
-//
-// var quotient = divide(number1, number2);
-// alert("The quotient is " + quotient);
